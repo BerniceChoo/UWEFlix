c513fa14117331afa961cb9b8e7588e7117348b8
Batman1010 <101429597+Batman1010@users.noreply.github.com>
2023-03-18 17:16:24 +0000

    Added update function for clubs

--- cinema_manager/urls.py
+++ cinema_manager/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.clubs_list, name='clubs-list'),
     path('create/', views.create_club, name='create-club'),
+    path('edit/<str:pk>/', views.edit_club, name='edit-club'),
     path('delete/<str:pk>/', views.delete_club, name='delete-club'),
 
 ]
--- cinema_manager/views.py
+++ cinema_manager/views.py
@@ -84,23 +84,50 @@ def create_club(request):
     }
     return render(request, 'cinema_manager/create.html', context)
 
-"""
-def edit_club(request, pk):
-    club = Club.objects.get(id=pk)
-    form = ClubForm(instance=club)
 
+def edit_club(request, pk):
+
+    club_id = ObjectId(pk)
+   
     if request.method == 'POST':
-        form = ClubForm(request.POST, request.FILES, instance=club)
-        if form.is_valid():
-            form.save()
+        clubname = request.POST['clubname']
+        city = request.POST['city']
+        street = request.POST['street']
+        email = request.POST['email']
+        houseno = request.POST['houseno']
+        phoneno = request.POST['phoneno']
+        postcode = request.POST['postcode']
+        telephoneno = request.POST['telephoneno']
+
+        document={"Name": clubname,
+                  "City": city,
+                  "Street": street,
+                  "Email": email,
+                  "HouseNumber": houseno,
+                  "PhoneNumber": phoneno,
+                  "PostCode": postcode,
+                  "TelephoneNumber": telephoneno,
+                    }
+        
+        result = Clubs.update_one({'_id': club_id},{'$set': document} )
+
+        if result.modified_count == 1:
+            # Document successfully updated
+            print(f"Document with _id '{club_id}' updated.")
             return redirect('clubs-list')
+        else:
+            # Document not found
+            print(f"No document found with _id '{club_id}'.")
+
+        return redirect('clubs-list')
+   
 
     context = {
-        'club': club,
-        'form': form,
+        'club': 1,
+        'form': 1,
     }
     return render(request, 'cinema_manager/edit.html', context)
-"""
+
 
 
 def delete_club(request, pk):
