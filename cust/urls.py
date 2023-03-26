5d27418e6160e669edbb9912ea15c66d76dee3ee
bernice <bernice@bernices-MacBook-Air.local>
2023-03-26 23:12:34 +0100

    edited urls

--- cust/urls.py
+++ cust/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from cust import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("home", views.home, name="home"),
+    #path('', views.dates_list, name='dates-list'),
+    #path('edit/<str:selectedDate>/', views.showings_list, name='showings-list'),
+    path('', views.select_date, name='select_date'),
+    path('view_data/', views.view_data, name='view_data')
+    #path('select_date/', views.showings_list, name='showings-list'),
+    # path('select_date/', views.select_date, name='select_date')
+    #path("", views.home, name="home"),
+    #path("home", views.home, name="home"),
 
-    path("selectDate/", views.selectDate, name="selectDate"),
-    path("selectDate/booking/<str:selectedDate>", views.booking, name="booking"),
-    path("payment/<int:adultQuantity>-<int:studentQuantity>-<int:childQuantity>-<int:selectedShowing>", views.payment, name="payment"),
+    #path("selectDate/", views.selectDate, name="selectDate"),
+    #path("selectDate/booking/<str:selectedDate>", views.booking, name="booking"),
+    #path("payment/<int:adultQuantity>-<int:studentQuantity>-<int:childQuantity>-<int:selectedShowing>", views.payment, name="payment"),
 ]
