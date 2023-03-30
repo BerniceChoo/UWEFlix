e6ba34903396b64ff87ee78d73f7f18e104b7e2f
bernice <bernice@bernices-MacBook-Air.local>
2023-03-30 01:11:29 +0100

    added enter tickets and booking

--- cust/urls.py
+++ cust/urls.py
@@ -2,16 +2,11 @@ from django.urls import path
 from cust import views
 
 urlpatterns = [
-    #path('', views.dates_list, name='dates-list'),
-    #path('edit/<str:selectedDate>/', views.showings_list, name='showings-list'),
     path('', views.select_date, name='select_date'),
-    path('view_data/', views.view_data, name='view_data')
-    #path('select_date/', views.showings_list, name='showings-list'),
-    # path('select_date/', views.select_date, name='select_date')
-    #path("", views.home, name="home"),
-    #path("home", views.home, name="home"),
+    path('view_data/', views.view_data, name='view_data'),
 
-    #path("selectDate/", views.selectDate, name="selectDate"),
-    #path("selectDate/booking/<str:selectedDate>", views.booking, name="booking"),
-    #path("payment/<int:adultQuantity>-<int:studentQuantity>-<int:childQuantity>-<int:selectedShowing>", views.payment, name="payment"),
+    path('view_data/ticket/', views.ticket, name='ticket'),
+    path('view_data/ticket/book_tickets/', views.book_tickets, name='book_tickets'),
+
+    path('view_data/ticket/book_tickets/booking_confirmation/', views.confirmation, name='confirmation'),
 ]
