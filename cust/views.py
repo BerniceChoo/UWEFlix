8abd3b1f65bb660d37643627251f2507d13a021b
bernice <bernice@bernices-MacBook-Air.local>
2023-03-31 16:02:15 +0100

    added no showing

--- cust/views.py
+++ cust/views.py
@@ -21,7 +21,8 @@ def view_data(request):
     selected_date = request.GET.get('date')
     results = Showings.find({'date': selected_date})
     movies = Showings.find({'date': selected_date}, {'filmTitle': 1})
-    context = {'results': results, 'selected_date': selected_date, 'movies': movies}
+    data = [doc for doc in results]
+    context = {'results': results, 'selected_date': selected_date, 'movies': movies, 'data': data}
     return render(request, 'cust/data_view.html', context)
 
 
@@ -38,6 +39,6 @@ def ticket(request):
 def book_tickets(request):
     selected_ticketsnum = request.GET.get('tickets')
     ticket_price = TicketsPrice.find_one()['customerPrice']
-    total_price = selected_ticketsnum * ticket_price
+    total_price = int(selected_ticketsnum) * ticket_price
     context = { 'selected_ticketsnum': selected_ticketsnum, 'ticket_price': ticket_price, 'total_price': total_price}
     return render(request, 'cust/book_tickets.html', context)
