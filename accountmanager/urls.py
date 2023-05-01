18286d050d98937ed7352432fe26e85abc5191c2
n23-miller <Nathan2.Miller@live.uwe.ac.uk>
2023-05-01 20:05:31 +0100

    Added first attempt at statments

--- accountmanager/urls.py
+++ accountmanager/urls.py
@@ -4,6 +4,7 @@ from accountmanager import views
 urlpatterns = [
     path("", views.accountMngrTable, name="home"),
     path("accountManagerTable", views.accountMngrTable, name="viewTable"),
+    path("accountManagerStatements", views.accountMngrStatements, name="viewStatements"),
     path("accountManagerAdd", views.accountMngrAdd, name="inputData"),
     path("accountManagerAddRecieved", views.accountMngrAddRecieved, name="recievedData"),
     path("accountManagerRemove", views.accountMngrRemove, name="removeAccount"),
